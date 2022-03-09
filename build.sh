f006ebc774cd19f8d845386300d3bdddc5ef5c41
Justme0 <jiangg2012@hotmail.com>
2022-03-10 02:39:40 +0800

    build: Get SDP dynamic candidate

--- build.sh
+++ build.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
 set -ex
+ifconfig
+date
+whoami
+w
 
+g_pwd=`pwd`
+trap 'cd $g_pwd' EXIT
+
+g_dst_dir=$(cd $(dirname ${BASH_SOURCE[0]}); pwd )
+# like golang defer
+cd $g_dst_dir
+
+# 1, run nodejs for web client demo
+cd webclient
+g_localip=`ifconfig  | head -n 2 | tail -n 1 | awk '{print $2}'`
+sed "s/CANDIDATE_PLACEHOLDER/${g_localip}/g" index.html.template > index.html
+# first kill, ignore error if not exist
+ps aux | grep "node index.js" | grep -v grep | awk '{print $2}' | xargs kill -9 || true
+# https://stackoverflow.com/questions/10408816/how-do-i-use-the-nohup-command-without-getting-nohup-out
+nohup bash -c "node index.js" </dev/null >/dev/null 2>&1 &
+cd $g_dst_dir
+
+# 2, compile server
+cd src
 kServerName="server_tywebrtc" # svr name is same in Makefile
 
 # force format code :)
@@ -14,7 +37,10 @@ make V=1
 
 # deploy
 # rsync --port=22222 -vzrtp --progress --password-file=/data/home/taylorjiang/rsync/rsync.pass $kServerName devsync@9.218.129.75::workspace || true
+cd $g_dst_dir
 
+# 3, run server
+cd src
 # Could move to a single file meaning run server
 runSvr()
 {
